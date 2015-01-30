b0c5728eabd3cd0ed2c486d5606878c19de3f705
mchrobot <martin.chrobot@telegraph.co.uk>
2015-01-30 07:16:40 +0000

    Update rstudio.rb

--- deploy/recipes/rstudio.rb
+++ deploy/recipes/rstudio.rb
@@ -1,3 +1,10 @@
+bash "download_r" do
+  
+  code <<-EOH
+    wget https://github.com/mchrobot/EC2Test/blob/master/rstudio-server-0.98.1091-x86_64.rpm
+  EOH
+end
+  
 rpm_package "rstudio-server-0.98.1091-x86_64.rpm" do
   action :install
   options "--nogpgcheck"
