e359007f5dc2e2fe663a68613b9de05ac6286f37
mchrobot <martin.chrobot@telegraph.co.uk>
2015-01-30 09:11:22 +0000

    Update rstudio.rb

--- deploy/recipes/rstudio.rb
+++ deploy/recipes/rstudio.rb
@@ -3,7 +3,7 @@ bash "download_r" do
   code <<-EOH
     wget http://download2.rstudio.org/rstudio-server-0.98.1091-x86_64.rpm
     cd /
-    yum install --nogpgcheck rstudio-server-0.98.1091-x86_64.rpm
+    yum install --nogpgcheck rstudio-server-0.98.1091-x86_64.rpm -y
   EOH
 end
 
