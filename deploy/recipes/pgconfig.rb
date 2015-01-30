24aa45040dd6d1dd0da603938081a8fad4d7e7e8
mchrobot <martin.chrobot@telegraph.co.uk>
2015-01-30 10:26:55 +0000

    Update pgconfig.rb

--- deploy/recipes/pgconfig.rb
+++ deploy/recipes/pgconfig.rb
@@ -3,6 +3,6 @@ bash "postgresconfig" do
   code <<-EOH
     cd /
     echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
-    chmod 0600 ~/.pgpass
+    chmod 0600 .pgpass
   EOH
 end
