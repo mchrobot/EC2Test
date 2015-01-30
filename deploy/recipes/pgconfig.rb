b4c0de9ca1208224c3719d96f694926a6f51c496
mchrobot <martin.chrobot@telegraph.co.uk>
2015-01-30 12:29:43 +0000

    Update pgconfig.rb

--- deploy/recipes/pgconfig.rb
+++ deploy/recipes/pgconfig.rb
@@ -5,15 +5,15 @@ bash "postgresconfig" do
     echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
     chmod 0600 .pgpass
     
-    cd /home/wtdatapull
+    cd /home/wtdatapull/
     echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
     chmod 0600 .pgpass
     
-    cd /home/shinyuser
+    cd /home/shinyuser/
     echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
     chmod 0600 .pgpass
     
-    cd /home/annualdash
+    cd /home/annualdash/
     echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
     chmod 0600 .pgpass
   EOH
