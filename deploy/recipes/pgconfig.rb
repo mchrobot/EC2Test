f442dabb1db57078914a18acfe13c9ba5ab38087
mchrobot <martin.chrobot@telegraph.co.uk>
2015-01-30 10:21:05 +0000

    Create pgconfig.rb

---
+++ deploy/recipes/pgconfig.rb
@@ -0,0 +1,8 @@
+bash "postgresconfig" do
+  
+  code <<-EOH
+    cd /
+    echo data-mart.cfhjjftfe0pb.eu-west-1.redshift.amazonaws.com:5439:webtrends:saltinig:W3btrends >> .pgpass
+    chmod 0600 ~/.pgpass
+  EOH
+end
