a411cf132c5f5f974209732ab2120946637ae9b8
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-11 12:43:13 +0000

    Update createAdmins.rb

--- deploy/recipes/createAdmins.rb
+++ deploy/recipes/createAdmins.rb
@@ -1,1 +1,14 @@
-test
+bash "createAdmins" do
+
+  code <<-EOH
+    
+    echo wtdatapull ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
+    echo shinyuser ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
+    echo annualdash ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
+    echo tdibudget ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
+    
+    echo chrobotm ALL=(ALL) NOPASSWD: ALL >> adminAccounts
+    echo glancm ALL=(ALL) NOPASSWD: ALL >> adminAccounts
+    
+  EOH
+end
