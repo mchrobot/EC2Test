46e887690093552cd04656cb1436b596920622ad
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-11 13:24:09 +0000

    Update passauth.rb

--- deploy/recipes/passauth.rb
+++ deploy/recipes/passauth.rb
@@ -1,1 +1,8 @@
-sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' sshd_config
+bash "passauth" do
+
+  code <<-EOH
+    
+    sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' sshd_config
+    
+  EOH
+end
