9f509a875aa9c3e41e6f7bd2f9a69017ad51294d
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-11 13:05:08 +0000

    Update createAdmins.rb

--- deploy/recipes/createAdmins.rb
+++ deploy/recipes/createAdmins.rb
@@ -2,13 +2,13 @@ bash "createAdmins" do
 
   code <<-EOH
     
-    echo wtdatapull ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
-    echo shinyuser ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
-    echo annualdash ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
-    echo tdibudget ALL=(ALL) NOPASSWD: ALL >> serviceAccounts
+    echo wtdatapull ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/serviceAccounts
+    echo shinyuser ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/serviceAccounts
+    echo annualdash ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/serviceAccounts
+    echo tdibudget ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/serviceAccounts
     
-    echo chrobotm ALL=(ALL) NOPASSWD: ALL >> adminAccounts
-    echo glancm ALL=(ALL) NOPASSWD: ALL >> adminAccounts
+    echo chrobotm ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/adminAccounts
+    echo glancm ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers.d/adminAccounts
     
   EOH
 end
