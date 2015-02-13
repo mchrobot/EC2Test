f7e518965d84e73dbf240f96a0593acdf212c57f
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 10:14:31 +0000

    Create eopsUsers.rb

---
+++ deploy/recipes/eopsUsers.rb
@@ -0,0 +1,11 @@
+user "chrobotm" do
+  home "/home/chrobotm"
+  shell "/bin/bash"
+  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
+end
+
+user "glancm" do
+  home "/home/glancm"
+  shell "/bin/bash"
+  password "$1$d3a8UxaW$f1bsCfxHAVjWTAQX7IOSZ/"
+end
