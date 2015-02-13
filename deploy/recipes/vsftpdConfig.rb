f684c97f62d6e23be72758979d71cb13aaa19f7c
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 14:59:11 +0000

    Update vsftpdConfig.rb

--- deploy/recipes/vsftpdConfig.rb
+++ deploy/recipes/vsftpdConfig.rb
@@ -1,10 +1,19 @@
-anonymous_enable=NO
-local_enable=YES
-chroot_local_user=YES
-service vsftpd restart
-chkconfig vsftpd on
+bash "mailxconfig" do
 
-echo "pasv_enable=YES" >> /etc/vsftpd.conf
-echo "pasv_min_port=64000" >> /etc/vsftpd.conf
-echo "pasv_max_port=64321" >> /etc/vsftpd.conf
-echo "port_enable=YES" >> /etc/vsftpd.conf
+  code <<-EOH
+
+    anonymous_enable=NO
+    local_enable=YES
+    chroot_local_user=YES
+    
+    service vsftpd restart
+    
+    chkconfig vsftpd on
+
+    echo "pasv_enable=YES" >> /etc/vsftpd.conf
+    echo "pasv_min_port=64000" >> /etc/vsftpd.conf
+    echo "pasv_max_port=64321" >> /etc/vsftpd.conf
+    echo "port_enable=YES" >> /etc/vsftpd.conf
+
+  EOH
+end
