e89619ed7781c43e14070e515d8f70323de25059
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 10:14:19 +0000

    Update createusers.rb

--- deploy/recipes/createusers.rb
+++ deploy/recipes/createusers.rb
@@ -33,15 +33,3 @@ user "annualdash" do
   shell "/bin/bash"
   password "$1$yAulmYU9$L9Bvu1dS1hUKWY6Z5436c."
 end
-
-user "chrobotm" do
-  home "/home/chrobotm"
-  shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
-end
-
-user "glancm" do
-  home "/home/glancm"
-  shell "/bin/bash"
-  password "$1$d3a8UxaW$f1bsCfxHAVjWTAQX7IOSZ/"
-end
