c48fb366386689843e30068e95de6f9b9791c1d0
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 10:10:49 +0000

    Update createusers.rb

--- deploy/recipes/createusers.rb
+++ deploy/recipes/createusers.rb
@@ -19,29 +19,17 @@ end
 user "wtdatapull" do
   home "/dev/sdf/wtdatapull"
   shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
+  password "$1$yCIxzifw$LHxTnuIsQg9W21fxtOeAC0"
 end
 
 user "shinyuser" do
   home "/dev/sdf/shinyuser"
   shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
+  password "$1$YRGY8RU0$hp6RO46PJ5Vc1p/.hqj7z/"
 end
 
 user "annualdash" do
   home "/dev/sdf/annualdash"
   shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
-end
-
-user "chrobotm" do
-  home "/home/chrobotm"
-  shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
-end
-
-user "glancm" do
-  home "/home/glancm"
-  shell "/bin/bash"
-  password "$1$Re1pPbv7$L/TIv1GFzkzJA2fFcZ16Y."
+  password "$1$yAulmYU9$L9Bvu1dS1hUKWY6Z5436c."
 end
