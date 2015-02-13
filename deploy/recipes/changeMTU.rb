0f12914f08621d7e5b8b27c5644644bdde7bcbd8
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 09:22:44 +0000

    Update changeMTU.rb

--- deploy/recipes/changeMTU.rb
+++ deploy/recipes/changeMTU.rb
@@ -1,10 +1,10 @@
 bash "changeMTU" do
 
   code <<-EOH
-
-   echo "sudo ip link set dev eth0 mtu 1500" >> /changeMTU.sh
-   chmod +x /changeMTU.sh
-   echo sh /changeMTU.sh >> /etc/rc.local
+    sudo ip link set dev eth0 mtu 1500
+    echo "sudo ip link set dev eth0 mtu 1500" >> /changeMTU.sh
+    chmod +x /changeMTU.sh
+    echo sh /changeMTU.sh >> /etc/rc.local
 
   EOH
 end
