f02ea9d20599ca7a5676cd40d539ab3b4dc4b777
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-13 15:18:07 +0000

    Update vsftpdConfig.rb

--- deploy/recipes/vsftpdConfig.rb
+++ deploy/recipes/vsftpdConfig.rb
@@ -6,10 +6,10 @@ bash "vsftpdConfig" do
     sed -i -e 's/local_enable=NO/local_enable=YES/' /etc/vsftpd/vsftpd.conf
     sed -i -e 's/#chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd/vsftpd.conf
     
-    echo "pasv_enable=YES" >> /etc/vsftpd.conf
-    echo "pasv_min_port=64000" >> /etc/vsftpd.conf
-    echo "pasv_max_port=64321" >> /etc/vsftpd.conf
-    echo "port_enable=YES" >> /etc/vsftpd.conf
+    echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "pasv_min_port=64000" >> /etc/vsftpd/vsftpd.conf
+    echo "pasv_max_port=64321" >> /etc/vsftpd/vsftpd.conf
+    echo "port_enable=YES" >> /etc/vsftpd/vsftpd.conf
 
     service vsftpd restart
     chkconfig vsftpd on
