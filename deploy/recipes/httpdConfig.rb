9a9566665f68db50909596e2aa5979a1702d8430
mchrobot <martin.chrobot@telegraph.co.uk>
2015-02-16 09:16:02 +0000

    Update httpdConfig.rb

--- deploy/recipes/httpdConfig.rb
+++ deploy/recipes/httpdConfig.rb
@@ -5,6 +5,7 @@ bash "httpdConfig" do
     mkdir www
     cd www
     mkdir html
+    cd /
     sed 's|DocumentRoot "/var/www/html"|DocumentRoot "/dev/xvd/www/html"|' /etc/httpd/conf/httpd.conf
     sed 's|<Directory "/var/www/html">|<Directory "/dev/xvd/www/html"> |' /etc/httpd/conf/httpd.conf
     
